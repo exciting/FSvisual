a8a78ee3b2ecae6db78bd816c9d85c9c552dc9fc
Jan Stutz <stutzjan@physik.hu-berlin.de>
2024-07-02 15:52:52 +0200

    inital commit

---
+++ fermi_surfaces.py
@@ -0,0 +1,227 @@
+import numpy as np
+import trimesh
+from skimage import measure
+
+
+def fraction(rez_lattice_vect, s_grid_size):
+    """
+    used in iterative process of generating the grid for create_mesh()
+    :param rez_lattice_vect: second or third rez. lattice vector
+    :param s_grid_size: number of datapoints the grid should have along the corresponding lattice vector
+    :return: list of s_grid_size points starting at 0 and adds rez_lattice_vect/s_grid_size for each entry plus the
+             entry before that
+    """
+    # fractions to go one grid-Point further each iteration (loop)
+    fraction_list = (np.ones((s_grid_size, 3)) * rez_lattice_vect / s_grid_size).tolist()
+    fraction_list[0] = [0, 0, 0]
+
+    helper_var = []
+    for entry in fraction_list:
+        if fraction_list.index(entry) == 0:
+            helper_var.append(entry)
+        else:
+            # adds the last value to the current value in the loop to increase the fraction by each iteration
+            helper_var.append((np.array(entry) + np.array(helper_var[-1])).tolist())
+    fraction_list = helper_var
+    return fraction_list
+
+
+def xc_malloc_tensor3f(x, y, z):
+    return np.zeros((x, y, z), dtype=float)
+
+
+def create_mesh(rez_lattice, grid_size, brillouin_zone):
+    """
+    creates a mesh within the reciprocal unit cell for any reciprocal lattice
+    creates second mesh (mol) where energy placeholders are stored to be replaced by real energy values
+    later on -> very important for fermi_surface shape
+
+    :param brillouin_zone: facets and vertices of the first BZ are needed
+    :param rez_lattice: koordinates for the reciprocal lattice vectors (list of three 3 Dimensional koordinates)
+    :param grid_size: number of datapoints the grid should have (list of 3 number for each lattice vector)
+    :return: standard mesh and mol mesh
+    """
+    grid_size = [int(grid_size_n) for grid_size_n in grid_size]
+    mesh = []
+
+    fraction1 = fraction(rez_lattice[1], grid_size[1])  # for generating basic mesh
+    fraction2 = fraction(rez_lattice[2], grid_size[2])
+
+    frmin = [1.0, 1.0, 1.0]  # placeholder for min and max vector of the fermi_surface
+    frmax = [-1.0, -1.0, -1.0]
+    Imin = [0] * 3
+    Imax = [0] * 3
+    frImin = [0.0] * 3
+    frImax = [0.0] * 3
+    band_grid = xc_malloc_tensor3f(grid_size[0], grid_size[1], grid_size[2])
+
+    # loop that creates the mesh
+    p = 0
+    for i in range(0, grid_size[0]):
+        for j in range(0, grid_size[1]):
+            for k in range(0, grid_size[2]):
+                v = np.array(rez_lattice[0]) / grid_size[0] * i + fraction1[j] + fraction2[k]
+                mesh.append(v)
+                band_grid[i, j, k] = p  # placeholder to create mol grid
+                p += 1
+
+    grid_size_minus_one = [grid - 1 for grid in grid_size]
+
+    for i in range(len(brillouin_zone[1])):  # facet
+        for j in range(len(brillouin_zone[1][i])):  # vertex
+            for k in range(3):  # finding real min and max vector of fermi_surface
+                a = (brillouin_zone[1][i][j][0] * rez_lattice[k][0]
+                     + brillouin_zone[1][i][j][1] * rez_lattice[k][1]
+                     + brillouin_zone[1][i][j][2] * rez_lattice[k][2])
+                if a < frmin[k]:
+                    frmin[k] = a
+                if a > frmax[k]:
+                    frmax[k] = a
+    for k in range(3):
+        Imin[k] = -grid_size_minus_one[k]
+        Imax[k] = grid_size_minus_one[k]
+
+        frImin[k] = float(Imin[k]) / grid_size_minus_one[k]
+        frImax[k] = float(Imax[k]) / grid_size_minus_one[k]
+
+    # creating the mols dictionary
+    mols = {'i': Imax[0] - Imin[0] + 1,
+            'j': Imax[1] - Imin[1] + 1,
+            'k': Imax[2] - Imin[2] + 1,
+            'lowcoor': [0, 0, 0],
+            "molgrid": xc_malloc_tensor3f(grid_size[0], grid_size[1], grid_size[2])}
+
+    for k in range(3):
+        mols['lowcoor'][k] = (frImin[0] * rez_lattice[k][0] +
+                              frImin[1] * rez_lattice[k][1] +
+                              frImin[2] * rez_lattice[k][2])
+        # for j in range(3):
+        #    mols['vec'][k][j] = (frImax[k] - frImin[k]) * rez_lattice[k][j]
+        #    mols['isoexpand']['rep_vec'][k][j] = rez_lattice[k][
+
+    for i1, i in enumerate(range(int(Imin[0] / 2), int(Imax[0] / 2) + 1)):
+        ii = i if i >= 0 else grid_size_minus_one[0] + i
+        for j1, j in enumerate(range(int(Imin[1] / 2), int(Imax[1] / 2) + 1)):
+            jj = j if j >= 0 else grid_size_minus_one[1] + j
+            for k1, k in enumerate(range(int(Imin[2] / 2), int(Imax[2] / 2) + 1)):
+                kk = k if k >= 0 else grid_size_minus_one[2] + k
+                mols['molgrid'][i1][j1][k1] = band_grid[ii, jj, kk]
+    return mesh, mols
+
+
+def crop_BZ(fermisurf, Brillouin_Zone):
+    # marching cubes:
+    triangulized = measure.marching_cubes(fermisurf, 0)
+
+    for triangle in triangulized:
+        for i in range(3):
+            index = triangle
+
+
+# def energy_selection(band_energies):
+
+
+def triangulate_faces(facets):
+    """
+    splits every facet of the 1. BZ into triangles for Trimesh to read -> function is used by brillouin_intersect_mesh()
+    :param facets: list of all facets with vertices counted from 0 to last vertex
+    :return:
+    """
+    triangles = []
+    for facet in facets:
+        if len(facet) == 3:
+            triangles.append(facet)
+        elif len(facet) == 4:
+            triangles.append([facet[0], facet[1], facet[2]])
+            triangles.append([facet[0], facet[2], facet[3]])
+        else:
+            # For facets with more than 4 vertices, use a fan triangulation
+            for i in range(1, len(facet) - 1):
+                triangles.append([facet[0], facet[i], facet[i + 1]])
+    return triangles
+
+
+def brillouin_intersect_mesh(brillouin_zone):
+    """
+    modulates a 3D mesh in shape of the 1. BZ to tell whether a point lies within the 1. BZ or not
+
+    :param brillouin_zone: list of all facets of the 1. BZ, within the facets shall be all vertices of the facet
+    :return: modulated 1. BZ as a Trimesh object
+    """
+    # creates a list with information on how the lines are connected
+    facet_order_list = []
+    helper_brillouin_zone = []
+
+    number_val_BZ = sum(len(facet) for facet in brillouin_zone)
+
+    j = 0
+    for facet in brillouin_zone:
+        facet_order = [i for i in range(j, j + len(facet)) if i < number_val_BZ + 1]
+        j += len(facet_order)
+        if len(facet_order) != 0:
+            facet_order_list.append(facet_order)
+        helper_brillouin_zone.extend(facet)
+    brillouin_zone = helper_brillouin_zone
+
+    # Triangulate the faces
+    triangulated_faces = triangulate_faces(facet_order_list)
+
+    brillouin_zone_object = trimesh.Trimesh(vertices=brillouin_zone, faces=triangulated_faces)
+
+    return brillouin_zone_object
+
+
+def fold_to_first_bz(mesh_point, brillouin_zone, bz_model):
+    # attempt for folding algorithm
+    # create plane:
+    for facet in brillouin_zone:
+        plane_vect1 = np.array(facet[0])
+        plane_vect2 = np.array(facet[1])
+        plane_vect3 = np.array(facet[2])
+
+        n = np.cross(plane_vect2, plane_vect3)
+
+        # plane in coordinate_form
+        # d = n1*v1 + n2*v2 + n3*v3
+        d = n[0] * plane_vect1[0] + n[1] * plane_vect1[1] + n[2] * plane_vect1[2]
+
+        # create line
+        # (mesh_point + x*n) in plane equation
+        a = np.dot(n, n)
+        b = d - np.dot(n, mesh_point)
+
+        # a*x = b
+
+        x = b / a
+
+        intersection_point = mesh_point + x * n
+
+        length = np.sqrt(np.dot(mesh_point - intersection_point, mesh_point - intersection_point))
+
+        # normalize n:
+        n_norm_const = np.sqrt(1 / np.dot(n, n))
+        n_norm = n * n_norm_const
+
+        # calculate mirrored point
+        p_mirror = mesh_point + 2 * length * n_norm
+
+        if bz_model.contains([p_mirror])[0]:
+            return p_mirror
+    return [0, 0, 0]
+
+
+def check_fermi_surface(energies, fermi_energy):
+    """
+    checks whether the energy in a band is equal to the fermi_energy (within uncertainties)
+    :param fermi_energy: energy value that FERMISURF.bxsf defines as fermiy_energy
+    :param energies: all energy values of one band
+    :return: list of indices for k-points to choose from the grid
+    """
+
+    index_for_k_position = []
+    i = 0
+    for energy in energies:
+        if fermi_energy - 0.01 <= energy <= fermi_energy + 0.01:
+            index_for_k_position.append(i)
+        i += 1
+    return index_for_k_position
---
+++ visualization_main.py
@@ -0,0 +1,215 @@
+import plotly.graph_objects as go
+from brilouin_zone import first_bz
+from fermi_surfaces import create_mesh, brillouin_intersect_mesh, crop_BZ, check_fermi_surface
+from input import read_energy_numbers
+import numpy as np
+from scipy.spatial import Delaunay
+import plotly.offline as pyo
+import pandas as pd
+
+
+source = "FERMISURF_Au_fcc.bxsf"
+data = read_energy_numbers(source)
+energy = data[0]
+fermi_energy = data[1]
+rez_base_vect = data[2]
+grid_size = data[3]
+
+# coordinates of the first Brillouin zone
+
+brillouin_zone = []
+brillouin_zone.extend(first_bz(rez_base_vect))
+x = brillouin_zone[0][0]
+y = brillouin_zone[0][1]
+z = brillouin_zone[0][2]
+
+# create standard mesh
+
+all_meshs = create_mesh(rez_base_vect, grid_size, brillouin_zone)
+new_mesh = all_meshs[0]
+new_mols = all_meshs[1]
+
+print("done")
+
+for columnName in energy.columns:
+    placeholder_energy = []
+    p = 0
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            for k in range(grid_size[2]):
+                #energy_list = energy[columnName].tolist()
+                placeholder_energy.append(energy[columnName][int(new_mols["molgrid"][i][j][k])])
+                new_mols["molgrid"][i][j][k] = energy["Band 4"][p]
+                if 0.01 >= energy["Band 4"][p] >= -0.01:
+                    new_mols["molgrid"][i][j][k] = 0
+                p += 1
+    energy[columnName] = placeholder_energy
+    print("done")
+
+# extract all k_points at fermi_energy
+# iterate through energy bands:
+k_points_dict = {}
+for (columnName, columnData) in energy.items():
+    k_point_indices = check_fermi_surface(columnData, fermi_energy)
+    k_points_list = [new_mesh[index].tolist() for index in k_point_indices]  # list of k_points for each band
+    k_points_dict[columnName] = k_points_list
+
+# calculate intersections
+
+# create 3d object of the first BZ
+mew_brillouin_zone_object = brillouin_intersect_mesh(brillouin_zone[1])
+
+scatter_fermi = []
+surface_bands = []
+for key, value in k_points_dict.items():
+    point_index = []
+    for point in range(len(value)):
+        k_points_dict[key][point][0] += -rez_base_vect[0][0] / 2
+        k_points_dict[key][point][1] += -rez_base_vect[0][0] / 2
+        k_points_dict[key][point][2] += -rez_base_vect[0][0] / 2
+
+        if mew_brillouin_zone_object.contains([value[point]])[0]:
+            pass
+        else:
+            # removes point outside 1. bz (for now)
+            # print(k_points_dict[key])
+            point_index.append(value[point])
+
+    for index in point_index:
+        k_points_dict[key].remove(index)
+
+    # plot of fermi_surfaces for each band
+
+    scatter_fermi.append(go.Scatter3d(x=[value[0] for value in k_points_dict[key]],
+                                      y=[value[1] for value in k_points_dict[key]],
+                                      z=[value[2] for value in k_points_dict[key]],
+                                      mode='markers'))
+    #if len(k_points_dict[key]) != 0:
+
+        # Sample data: replace these lists with your actual data
+
+
+from scipy.interpolate import Rbf
+import plotly.io as pio
+
+"""
+
+x = [value[0] for value in k_points_dict["Band 4"]]
+y = [value[1] for value in k_points_dict["Band 4"]]
+z = [value[2] for value in k_points_dict["Band 4"]]
+
+X,Y = np.meshgrid(x,y)
+
+# Convert spherical coordinates to Cartesian coordinates
+
+# Create the 3D mesh plot
+fig = go.Figure(data=[go.Mesh3d(
+    x=X,
+    y=Y,
+    z=z,
+    color='lightblue',
+    opacity=0.50
+
+)])
+
+# Update the layout
+fig.update_layout(
+    scene=dict(
+        xaxis_title='X Axis',
+        yaxis_title='Y Axis',
+        zaxis_title='Z Axis',
+    ),
+    title='Irregular Sphere 3D Mesh Plot',
+    autosize=True
+)
+
+# Show the plot
+fig.show()
+
+
+"""
+
+"""
+# Visualization
+# visualization of the brillouin_zone
+
+# Create a 3D scatter plot
+scatter_BZ = go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode='lines',
+    line=dict(color='black', width=2)
+)
+
+# contains all
+fig_data = [scatter_BZ]
+fig_data.extend(scatter_fermi)
+
+fig = go.Figure(data=fig_data)
+
+# Define the layout of the plot
+fig.update_layout(
+    scene=dict(
+        xaxis_title='kx',
+        yaxis_title='ky',
+        zaxis_title='kz',
+        aspectmode='cube'
+    )
+)
+
+# Show the plot
+# axis ranges
+x_range = [-np.abs(rez_base_vect[0][0]), np.abs(rez_base_vect[0][0])]
+y_range = [-np.abs(rez_base_vect[0][0]), np.abs(rez_base_vect[0][0])]
+z_range = [-np.abs(rez_base_vect[0][0]), np.abs(rez_base_vect[0][0])]
+
+
+fig.update_layout(
+    scene=dict(
+        xaxis=dict(visible=False, range=x_range),
+        yaxis=dict(visible=False, range=y_range),
+        zaxis=dict(visible=False, range=z_range),
+        annotations=[],  # Remove any annotations if present
+        aspectmode='cube',
+        camera=dict(
+            projection=dict(
+                type='orthographic'
+                # to change the perspective (so that lines dont distort over distance (nicht verjüngen))
+            )
+        )
+    )
+)
+
+fig.show()
+"""
+import numpy as np
+from skimage import measure
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+grid_size = 41
+#energy_values = np.random.random((grid_size, grid_size, grid_size))  # Replace with your actual energy values
+energy_values = new_mols["molgrid"]
+# Define the isovalue for the surface (this value should represent the energy level that forms the surface)
+isovalue = 0.0  # Adjust this value based on your data
+
+# Apply the Marching Cubes algorithm
+vertices, faces, normals, values = measure.marching_cubes(energy_values)
+
+# Plot the resulting surface
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a Poly3DCollection from the vertices and faces
+mesh = Poly3DCollection(vertices[faces], alpha=0.7)
+mesh.set_facecolor('cyan')
+mesh.set_edgecolor('k')
+ax.add_collection3d(mesh)
+
+# Set plot limits
+ax.set_xlim(0, grid_size - 1)
+ax.set_ylim(0, grid_size - 1)
+ax.set_zlim(0, grid_size - 1)
+
+plt.show()
