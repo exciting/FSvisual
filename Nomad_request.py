ba00119121106b50c3f95534b55be9214c744f42
Jan Stutz <stutzjan@physik.hu-berlin.de>
2024-11-28 09:40:18 +0100

    first iteration to download .bxsf files from NOMAD

---
+++ Nomad_request.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import os
+import random
+
+base_url = 'http://nomad-lab.eu/prod/v1/api/v1'
+
+response = requests.post(
+    f'{base_url}/entries/query',
+    json={
+        #"owner":"visible","query":{"and":[{"results.method.simulation.program_name:any":["exciting"],"authors.name:any":["Qiang Fu"]},{"quantities:all":["results.method.simulation.program_name"]}]},"aggregations":{"results.material.elements:widget":{"terms":{"exclude_from_search":False,"update":True,"size":119,"type":"terms","changed":True,"quantity":"results.material.elements"}},"results.material.symmetry.space_group_symbol:1":{"terms":{"exclude_from_search":True,"update":True,"size":3,"type":"terms","changed":True,"quantity":"results.material.symmetry.space_group_symbol"}},"results.material.structural_type:2":{"terms":{"exclude_from_search":True,"update":True,"size":5,"type":"terms","include":["1D","2D","atom","bulk","molecule / cluster","surface"],"changed":True,"quantity":"results.material.structural_type"}},"results.method.simulation.program_name:3":{"terms":{"exclude_from_search":True,"update":True,"size":3,"type":"terms","changed":True,"quantity":"results.method.simulation.program_name"}},"results.material.symmetry.crystal_system:4":{"terms":{"exclude_from_search":True,"update":True,"size":5,"type":"terms","changed":True,"quantity":"results.material.symmetry.crystal_system"}},"upload_create_time:default_histogram":{"histogram":{"exclude_from_search":True,"update":True,"type":"histogram","buckets":30,"changed":True,"quantity":"upload_create_time"}},"external_db:scroll":{"terms":{"exclude_from_search":True,"update":True,"size":5,"type":"terms","changed":True,"quantity":"external_db"}},"datasets.dataset_name:scroll":{"terms":{"exclude_from_search":True,"update":True,"size":10,"type":"terms","changed":True,"quantity":"datasets.dataset_name"}}},"pagination":{"order_by":"upload_create_time","order":"desc","page_size":10},"required":{"exclude":["quantities","sections"]}
+        "owner":"visible","query":{"and":[{"results.method.simulation.program_name:any":["exciting"],"authors.name:any":["Qiang Fu"],"results.material.elements:all":["Ti"]},{"quantities:all":["results.method.simulation.program_name"]}]},"aggregations":{"results.material.elements:widget":{"terms":{"exclude_from_search":False,"update":True,"size":119,"type":"terms","changed":True,"quantity":"results.material.elements"}},"results.material.symmetry.space_group_symbol:1":{"terms":{"exclude_from_search":True,"update":True,"size":5,"type":"terms","changed":True,"quantity":"results.material.symmetry.space_group_symbol"}},"results.material.structural_type:2":{"terms":{"exclude_from_search":True,"update":True,"size":7,"type":"terms","include":["1D","2D","atom","bulk","molecule / cluster","surface"],"changed":True,"quantity":"results.material.structural_type"}},"results.method.simulation.program_name:3":{"terms":{"exclude_from_search":True,"update":True,"size":5,"type":"terms","changed":True,"quantity":"results.method.simulation.program_name"}},"results.material.symmetry.crystal_system:4":{"terms":{"exclude_from_search":True,"update":True,"size":7,"type":"terms","changed":True,"quantity":"results.material.symmetry.crystal_system"}}},"pagination":{"page_size":100,"order_by":"upload_create_time","order":"desc","total":151},"required":{"exclude":["quantities","sections"]}
+    })
+
+data = response.json()["data"]
+for entry in data:
+    files = entry["files"]
+    files = [file for file in files if file.endswith(".bxsf")]
+
+    if len(files) == 1:
+        bxsf_file = files[0]
+        bxsf_file = os.path.basename(bxsf_file)
+        method = entry["results"]["method"]["simulation"]["dft"]["xc_functional_type"]
+        entry_id = entry["entry_id"]
+
+        # Dateipfad
+        if method == "GGA":
+            file_path = f'E:/Fermisurfaces/GGA/{random.randint(1, 10)}{bxsf_file}'
+        elif method == "LDA":
+            file_path = f'E:/Fermisurfaces/LDA/{random.randint(1, 10)}{bxsf_file}'
+        else:
+            file_path = f'E:/Fermisurfaces/unknown/{random.randint(1, 10)}{bxsf_file}'
+
+        try:
+            # Datei herunterladen.
+            response2 = requests.get(f'https://nomad-lab.eu/prod/v1/api/v1/entries/{entry_id}/raw/{bxsf_file}?offset=0&length=500000000&decompress=true', stream=True)
+            response.raise_for_status()
+
+            # Datei speichern
+            with open(file_path, "wb") as file:
+                for chunk in response2.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+            print(f"Datei wurde erfolgreich gespeichert: {file_path}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Fehler beim Herunterladen: {e}")
+
+
+
+
+
+
+
